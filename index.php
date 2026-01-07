fc73d6dfdc468cdd2a55cc7ad68a8efcd5ed3c43
SinaniFiona12 <r1049215@student.thomasmore.be>
2026-01-07 22:33:32 +0100

    Feat: Create index page skeleton

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Fiona's Flowershop</title>
+
+  <link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+
+</body>
+</html>
